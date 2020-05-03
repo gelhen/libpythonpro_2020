5c71436d9023d3424ba0c135fc8c4e3f04967299
gelhen <gelhen@gmail.com>
2020-05-03 20:21:26 -0300

    Implementado a utilização do modulo Mock

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuario.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuario.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock
+
 import pytest
 
 from libpythonpro.spam.enviador_de_email import Enviador
@@ -12,21 +14,40 @@ from libpythonpro.spam.modelos import Usuario
          Usuario(nome='Marcos', email='gelhen@gmail.com')
          ]
         ,
-        [Usuario(nome='Andre', email='gelhen@gmail.com')
-         ]
+        [
+            Usuario(nome='Andre', email='gelhen@gmail.com')
+        ]
     ]
 )
 def test_qte_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = Enviador()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_email(
         'gelhen@gmail.com',
         'Curso Python Pro',
         'Confira os Módulos Fantasticos'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Andre', email='gelhen@gmail.com')
+    sessao.salvar(usuario)
+    enviador = Mock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_email(
+        'gelhen@gmail.com',
+        'Curso Python Pro',
+        'Confira os Módulos Fantasticos'
+    )
+    enviador.enviar.assert_called_once_with(
+        'gelhen@gmail.com',
+        'gelhen@gmail.com',
+        'Curso Python Pro',
+        'Confira os Módulos Fantasticos'
+    )
 
 
 def test_envio_de_spam(sessao):
